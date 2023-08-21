44ce4bef8df9eab3fbe04e8f9bb96a9485f2009f
midasacademy <142730731+midasacademy@users.noreply.github.com>
2023-08-21 18:30:36 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 require('dotenv').config();
 // var plm = require('passport-local-mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/midas')
 mongoose.connect(`mongodb+srv://midasenglishacademy03:${process.env['mongo-user-pwd']}@cluster0.9kiiusr.mongodb.net/?retryWrites=true&w=majority`)
 
 var userSchema = mongoose.Schema({
